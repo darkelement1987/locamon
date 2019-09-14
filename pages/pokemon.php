fd6a791ddee553e81ff7250609ae56aed84d562b
AboveColin <colindv2000@gmail.com>
2019-09-14 21:13:08 +0200

    Update pokemon.php

--- pages/pokemon.php
+++ pages/pokemon.php
@@ -1,4 +1,36 @@
 <?php
 include '../config/functions.php';
-monMad();
 ?>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.2/css/responsive.bootstrap4.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/scroller/2.0.0/css/scroller.bootstrap4.css"/>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.2/js/dataTables.responsive.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/scroller/2.0.0/js/dataTables.scroller.js"></script>
+<script>$(document).ready( function () {
+    $('#example').DataTable();
+} );</script>
+<h3>Locamon <b>POKEMON</b></h3>
+<br>
+<br>
+<table id="example" class="table table-striped table-bordered" style="width:100%">
+    <thead>
+        <tr>
+            <th>Pokemon</th>
+            <th>lat, lon</th>
+            <th>cp</th>
+            <th>disappear_time</th>
+            <th>encounter_id</th>
+        </tr>
+    </thead>
+    <tbody>
+            <?php
+                monMad();
+            ?>
+    </tbody>
+</table>
