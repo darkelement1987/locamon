ed555ba2b70e0f03db3927b322b4a8243a25f98b
darkelement1987 <jgernaat1987@gmail.com>
2019-09-14 09:52:25 +0200

    Make basic config

--- config/config.php
+++ config/config.php
@@ -1,1 +1,17 @@
-#
+<?php
+
+// Connect to Database
+$servername = "servername";
+$username = "username";
+$password = "password";
+$database = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
