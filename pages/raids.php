4ba97b4f10208c132f9bb080434060b2ed57bc12
AboveColin <colindv2000@gmail.com>
2019-09-14 21:23:23 +0200

    Update raids.php

--- pages/raids.php
+++ pages/raids.php
@@ -3,25 +3,17 @@ include '../config/functions.php';
 ?>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css"/>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.css"/>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.2/css/responsive.bootstrap4.css"/>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/scroller/2.0.0/css/scroller.bootstrap4.css"/>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.2/js/dataTables.responsive.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/scroller/2.0.0/js/dataTables.scroller.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.18/datatables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.18/datatables.min.js"></script>
 <script>$(document).ready( function () {
-    $('#example').DataTable();
+    $('#raid_table').DataTable();
 } );</script>
 </head>
+
 <h3>Locamon <b>RAIDS</b></h3>
 <br>
 <br>
-<table id="example" class="table table-striped table-bordered" style="width:100%">
+<table id="raid_table" class="table table-striped table-bordered" style="width:100%">
     <thead>
         <tr>
             <th>Raid Boss</th>
