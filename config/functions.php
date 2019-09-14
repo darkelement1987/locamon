8166c8f514461ac51fc856d8344cc616d93e06fc
AboveColin <colindv2000@gmail.com>
2019-09-14 22:41:53 +0200

    Update functions.php

--- config/functions.php
+++ config/functions.php
@@ -1,13 +1,16 @@
 <?php
 function monMad() {
+  $mon_name = json_decode(file_get_contents('../json/pokedex.json'), true);
   require('config.php');
-  $result = $conn->query("SELECT * FROM pokemon WHERE disappear_time > utc_timestamp() ORDER BY disappear_time ASC;");
+  $result = $conn->query("SELECT * FROM pokemon WHERE disappear_time > utc_timestamp()");
   if (mysqli_num_rows($result) > 0) {
       while($row = mysqli_fetch_assoc($result)) {
           $disappear_time = explode(" ", $row["disappear_time"]);
+          $iv = round((($row["individual_attack"]+$row["individual_defence"]+$row["individual_stamina"])/45)*100,2);
           echo "<tr>";
-          echo "<td> <img height='42' width='42' src='" . $assetRepo . "pokemon_icon_"; if ($row["pokemon_id"] < 100 && $row["pokemon_id"] > 9) { echo '0' . $row["pokemon_id"]; } elseif ($row["pokemon_id"] < 10) { echo '00' . $row["pokemon_id"]; } else { echo $row["pokemon_id"]; }; echo "_00.png'</td>";
+          echo "<td> <img height='42' width='42' src='" . $assetRepo . "pokemon_icon_"; if ($row["pokemon_id"] < 100 && $row["pokemon_id"] > 9) { echo '0' . $row["pokemon_id"]; } elseif ($row["pokemon_id"] < 10) { echo '00' . $row["pokemon_id"]; } else { echo $row["pokemon_id"]; }; echo "_00.png'</img> " . $mon_name[$row["pokemon_id"]][name] ."</td>";
           echo "<td> <a href='https://maps.google.com/?q=" . $row["latitude"] . "," . $row["longitude"] . "'>MAP</a></td>";
+          echo "<td> " . $iv . "%</td>";
           echo "<td> " . $row["cp"] . "</td>";
           echo "<td> " . $disappear_time[1] . "</td>";
           echo "<td> " . $row["encounter_id"] . "</td>";
