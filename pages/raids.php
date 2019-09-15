c03ac2c30436326613cfe14e5010d92bffc5ab51
darkelement1987 <jgernaat1987@gmail.com>
2019-09-15 08:29:23 +0200

    Add files via upload

--- pages/raids.php
+++ pages/raids.php
@@ -1,6 +1,8 @@
 <?php
 include '../config/functions.php';
 ?>
+<!DOCTYPE html>
+<html>
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.1.1/jq-3.3.1/dt-1.10.18/datatables.min.css"/>
@@ -9,7 +11,7 @@ include '../config/functions.php';
     $('#raid_table').DataTable();
 } );</script>
 </head>
-
+<body>
 <h3>Locamon <b>RAIDS</b></h3>
 <br>
 <br>
@@ -29,3 +31,5 @@ include '../config/functions.php';
             ?>
     </tbody>
 </table>
+</body>
+</html>
