45d38d6584eab3cd1cc2fe79f2663648a607a7f8
darkelement1987 <jgernaat1987@gmail.com>
2019-09-17 16:49:56 +0200

    Remove local gen5 code. forgot to remove

--- config/functions_mad.php
+++ config/functions_mad.php
@@ -29,36 +29,6 @@ function getMons() {
   }
 }
 
-function getGen5() {
-    $mon_name = json_decode(file_get_contents('../json/pokedex.json'), true);
-    require('config.php');
-    $result = $conn->query("SELECT * FROM pokemon WHERE disappear_time > utc_timestamp() and pokemon_id>493");
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_object()) {
-			$cp = $row->cp;
-            $iv = round((($row->individual_attack+$row->individual_defense+$row->individual_stamina)/45)*100,2) . '%';
-			if ($cp==NULL){$cp='-';$iv='-';} else {$cp = $row->cp . ' CP';}
-			?>
-            <tr>
-                <td> 
-                    <img height='42' width='42' 	
-                    src='<?=$assetRepo?>pokemon_icon_<?=str_pad($row->pokemon_id, 3, 0, STR_PAD_LEFT)?>_00.png' />
-                    <?=$mon_name[$row->pokemon_id]['name']?>
-                </td>
-                <td><?=$iv?></td>
-                <td><?=$cp?></td>
-                <td><?=$row->disappear_time?></td>
-                <td><?=$row->last_modified?></td>
-                <td> 
-                    <a href='https://maps.google.com/?q=<?=$row->latitude . ','. $row->longitude?>'>MAP</a>
-                </td>
-            </tr>
-     <?php }
-  } else {
-      echo "ERROR: No Results Found!";
-  }
-}
-
 function getRaids() {
 	$raid_mon_name = json_decode(file_get_contents('../json/pokedex.json'), true);
   require('config.php');
