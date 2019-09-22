b1a6d61335370eabb4002a12c597f64098565381
darkelement1987 <jgernaat1987@gmail.com>
2019-09-23 00:24:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,19 +21,13 @@ if (!empty($_GET['page'])) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <h3><?= $title ?></h3>
-    <br>
-    <br>
+    <p>[<a class="link" href="./index.php?page=pokemon">POKEMON</a> | <a class="link" href="./index.php?page=raids">RAIDS</a> | <a class="link" href="./index.php?page=ditto">DITTO</a>]</p>
     <div class="table-container">
         <?php
         index();
         ?>
     </div>
     <!-- TEMP LINKS TO THE PAGES -->
-    <p><a class="link" href="./index.php?page=raids">RAIDS</a></p>
-    <p><a class="link" href="./index.php?page=pokemon">MONS</a></p>
-
-
-
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/fh-3.1.4/r-2.2.2/datatables.min.js"></script>
     <script src='./js/update.js'></script>
 
