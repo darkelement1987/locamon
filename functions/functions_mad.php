bae640a45b4d9bd1fd319aae59b1336cf03bcf77
John Bieri <littlemantis90@gmail.com>
2019-10-14 17:43:11 -0400

    Working on it
    Finally getting around to this push.  [this is to be considered alpha ~ beta ]
    Api for Js interaction with database,
    Moved from php rendering of tables to JS with Ajax
    Tables auto remove and add new pokemon/raids
    child rows on tables
    added static maps for looks
    countdowns
    added fontawesome5 for icons
    Modeled after bootstrap 4 framework
    Added script to scrape for data jsons
    
    ## Todo ##
    Developers notes
    Break apart get functions into parts for readability/speed
    Add in some more of Darks Beta
    Filter buttons and functions

--- functions/functions_mad.php
+++ functions/functions_mad.php
@@ -1,74 +1,156 @@
 <?php
-function getMons()
+function getMons($u = null)
 {
     global $conn;
     global $assetRepo;
+    global $mapkey;
+
     $mons = [];
-    $mon_name = json_decode(file_get_contents(DIRECTORY . '/json/pokedex.json'), true);
-    $sql = "SELECT individual_attack, individual_defense, individual_stamina, pokemon_id, cp, UNIX_TIMESTAMP(CONVERT_TZ(disappear_time, '+00:00', @@global.time_zone)) as disappear_time, UNIX_TIMESTAMP(CONVERT_TZ(last_modified, '+00:00', @@global.time_zone)) as last_modified, latitude, longitude  FROM pokemon WHERE disappear_time > utc_timestamp();";
+    $pokedex = json_decode(file_get_contents(DIRECTORY . '/json/pokedex.json'));
+    $key = json_decode(file_get_contents(DIRECTORY . '/json/forms.json'));
+    $moves = json_decode(file_get_contents(DIRECTORY . '/json/moves.json'));
+    $sql = <<<SQL
+    SELECT
+        pokemon.encounter_id as id,
+        pokemon.latitude as lat, 
+        pokemon.longitude as lon,
+        pokemon.height,
+        pokemon.weight,
+        UNIX_TIMESTAMP(CONVERT_TZ(pokemon.disappear_time, '+00:00', @@global.time_zone)) as expires,
+        UNIX_TIMESTAMP(CONVERT_TZ(pokemon.last_modified, '+00:00', @@global.time_zone)) as updated, 
+        pokemon.individual_attack as atk_iv, 
+        pokemon.individual_defense as def_iv, 
+        pokemon.individual_stamina as sta_iv, 
+        pokemon.pokemon_id,
+        pokemon.move_1,
+        pokemon.move_2,
+        pokemon.gender,
+        pokemon.cp,
+        pokemon.cp_multiplier,
+        pokemon.form,
+        pokemon.costume,
+        pokemon.catch_prob_1,
+        pokemon.catch_prob_2,
+        pokemon.catch_prob_3
+    FROM pokemon 
+    WHERE pokemon.disappear_time > utc_timestamp();
+SQL;
+    if (!empty($u)) {
+        $sql = $sql . ' AND pokemon.last_modified > DATE_SUB(UTC_TIMESTAMP(), INTERVAL ' . $u . ' SECOND)';
+    }
     $result = $conn->query($sql);
     if ($result && $result->num_rows > 0) {
         while ($row = $result->fetch_object()) {
-            if ($row->individual_attack !== null && $row->individual_defense !== null &&  $row->individual_stamina !== null) {
-                $row->iv = round((($row->individual_attack + $row->individual_defense + $row->individual_stamina) / 45) * 100, 2) . '%';
+            if ($row->atk_iv !== null && $row->def_iv !== null &&  $row->sta_iv !== null) {
+                $row->iv = round((($row->atk_iv + $row->def_iv + $row->sta_iv) / 45) * 100, 2);
             } else {
                 $row->iv = '-';
 				$row->cp = '-';
             }
-            $row->sprite = $assetRepo . 'pokemon_icon_' . str_pad($row->pokemon_id, 3, 0, STR_PAD_LEFT) . '_00.png';
-            $row->name = $mon_name[$row->pokemon_id]['name'];
-            $mons[] = $row;
-        }
-        return $mons;
-    } else {
-        return '<tr><td colspan="6" class="text-center"> No Pokemon At This Time</td></tr>';
-    }
-}
-
-function getDitto()
-{
-    global $conn;
-    global $assetRepo;
-    $mons = [];
-    $mon_name = json_decode(file_get_contents(DIRECTORY . '/json/pokedex.json'), true);
-    $sql = "SELECT individual_attack, individual_defense, individual_stamina, pokemon_id, cp, UNIX_TIMESTAMP(CONVERT_TZ(disappear_time, '+00:00', @@global.time_zone)) as disappear_time, UNIX_TIMESTAMP(CONVERT_TZ(last_modified, '+00:00', @@global.time_zone)) as last_modified, latitude, longitude  FROM pokemon WHERE disappear_time > utc_timestamp() AND pokemon_id in (13,46,48,163,165,167,187,223,273,293,300,316,322,399) AND weather_boosted_condition > 0 AND (individual_attack < 4 OR individual_defense < 4 OR individual_stamina < 4 OR cp_multiplier < .3);";
-    $result = $conn->query($sql);
-    if ($result && $result->num_rows > 0) {
-        while ($row = $result->fetch_object()) {
-            if ($row->individual_attack !== null && $row->individual_defense !== null &&  $row->individual_stamina !== null) {
-                $row->iv = round((($row->individual_attack + $row->individual_defense + $row->individual_stamina) / 45) * 100, 2) . '%';
+            if (empty($row->cp_multiplier)) {
+                $row->level = '-';
             } else {
-                $row->iv = '-';
-				$row->cp = '-';
+                if ($row->cp_multiplier < 0.73) {
+                    $level = 58.35178527 * $row->cp_multiplier * $row->cp_multiplier - 2.838007664 * $row->cp_multiplier + 0.8539209906;
+                } elseif ($row->cp_multiplier > 0.73) {
+                    $level = 171.0112688 * $row->cp_multiplier - 95.20425243;
+                }
+                $row->level = (round($level) * 2) / 2;
             }
-            $row->sprite = $assetRepo . 'pokemon_icon_132_00.png';
-            $row->name = 'Ditto';
+            if (!empty($row->move_1) && !empty($row->move_2)) {
+            $row->move_1 = $moves->{$row->move_1};
+            $row->move_2 = $moves->{$row->move_2};
+            }
+            $row->disappear_time = date('g:i:s', $row->expires);
+            $row->last_modified = date('g:i:s', $row->updated);
+            $row->sprite = $assetRepo . 'pokemon_icon_' . str_pad($row->pokemon_id, 3, '0', STR_PAD_LEFT) . '_' . str_pad($row->form, 2, '0', STR_PAD_LEFT) . '.png';
+            $row->name = $pokedex->{$row->pokemon_id}->name;
+            $row->types = $pokedex->{$row->pokemon_id}->types;
+            if (!empty($row->form) || !$row->form === '0') {
+                $row->form = $key->forms->{$row->form};
+            }
+            $row->static_map = '';
+            if ($mapkey !== '') {
+                $row->static_map = 'https://open.mapquestapi.com/staticmap/v5/map?size=300,200&zoom=15&locations=' . $row->lat . ',' . $row->lon . '|https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' . $row->pokemon_id . '.png&key=' . $mapkey;
+            }
+
             $mons[] = $row;
         }
-        return $mons;
-    } else {
-        return '<tr><td colspan="6" class="text-center"> No Ditto At This Time</td></tr>';
     }
 }
 
-function getRaids()
+function getRaids($u = null)
 {
     global $conn;
     global $assetRepo;
-	global $clock;
+    global $mapkey;
     $raids = [];
-    $sql = "SELECT UNIX_TIMESTAMP(CONVERT_TZ(a.start, '+00:00', @@global.time_zone)) as start, UNIX_TIMESTAMP(CONVERT_TZ(a.end, '+00:00', @@global.time_zone)) as end, UNIX_TIMESTAMP(CONVERT_TZ(a.spawn, '+00:00', @@global.time_zone)) as spawn, a.pokemon_id, a.move_1, a.move_2, UNIX_TIMESTAMP(CONVERT_TZ(a.last_scanned, '+00:00', @@global.time_zone)) as last_scanned, b.name, a.level, a.cp, c.latitude, c.longitude FROM raid a INNER JOIN gymdetails b INNER JOIN gym c ON a.gym_id = b.gym_id AND a.gym_id = c.gym_id  AND a.end > UTC_TIMESTAMP() ORDER BY a.end ASC";
+    $pokedex = json_decode(file_get_contents(DIRECTORY . '/json/pokedex.json'));
+    $key = json_decode(file_get_contents(DIRECTORY . '/json/pokemon.json'));
+    $moves = json_decode(file_get_contents(DIRECTORY . '/json/moves.json'));
+    $sql = <<<SQL
+        SELECT 
+            c.gym_id as id,
+            c.latitude as lat, 
+            c.longitude as lon,
+            b.name as gym_name,
+            b.url as image,
+            UNIX_TIMESTAMP(CONVERT_TZ(a.end, '+00:00', @@global.time_zone)) as end, 
+            UNIX_TIMESTAMP(CONVERT_TZ(a.start, '+00:00', @@global.time_zone)) as start,
+            UNIX_TIMESTAMP(CONVERT_TZ(a.spawn, '+00:00', @@global.time_zone)) as spawn, 
+            a.pokemon_id, 
+            a.move_1, 
+            a.move_2, 
+            UNIX_TIMESTAMP(CONVERT_TZ(a.last_scanned, '+00:00', @@global.time_zone))          as last_scanned, 
+            a.form, 
+            c.team_id, 
+            c.slots_available, 
+            a.level, 
+            c.is_ex_raid_eligible as ex,
+            a.cp, 
+            b.description
+        FROM raid a INNER JOIN gymdetails b INNER JOIN gym c 
+        ON a.gym_id = b.gym_id AND a.gym_id = c.gym_id
+        WHERE a.end > utc_timestamp()
+SQL;
+    if (!empty($u)) {
+        $sql = $sql . ' a.last_scanned > DATE_SUB(UTC_TIMESTAMP(), INTERVAL ' . $u . ' SECOND)';
+    }
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_object()) {
-            $row->time_start = date($clock, $row->start);
-            $row->time_end = date($clock, $row->end);
-            $row->raid_scan_time = date($clock, $row->last_scanned);
-            $row->sprite = $assetRepo . 'pokemon_icon_' . str_pad($row->pokemon_id, 0, 3, STR_PAD_LEFT) . '_00.png';
+            if (!empty($row->form)) {
+                $row->form = $key->forms->{$row->form};
+            }
+            if (!empty($row->move_1) && !empty($row->move_2)) {
+                $row->move_1 = $moves->{$row->move_1};
+                $row->move_2 = $moves->{$row->move_2};
+                }
+            $row->time_start = date('g:i:s', $row->start);
+            $row->time_spawn = date('g:i:s', $row->spawn);
+            $row->time_end = date('g:i:s', $row->end);
+            $row->raid_scan_time = date('g:i:s', $row->last_scanned);
+            $row->stars = str_repeat('★', $row->level);
+
+            if ($row->gym_name === null) {
+                $row->gym_name = 'Link';
+            }
+            if (!empty($row->pokemon_id)) {
+                $id = $row->pokemon_id;
+                $row->types = $pokedex->$id->types;
+                $row->sprite = $assetRepo . 'pokemon_icon_' . str_pad($row->pokemon_id, 3, '0', STR_PAD_LEFT) . '_' . str_pad($row->form, 2, '0', STR_PAD_LEFT) . '.png';
+                $row->name = $pokedex->{$row->pokemon_id}->name;
+            } else {
+                $id = '0';
+                $row->sprite = 'images/egg_' . $row->level . '.png';
+                $row->name = 'Egg';
+            }
+            $row->static_map = '';
+            if ($mapkey !== '') {
+                $row->static_map = 'https://open.mapquestapi.com/staticmap/v5/map?size=400,200&zoom=16&locations=' . $row->lat . ',' . $row->lon . '|https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' . $id . '.png&key=' . $mapkey;
+            }
             $raids[] = $row;
         }
-        return $raids;
-    } else {
-        return '<tr><td colspan="6" class="text-center"> No Raids At This Time</td></tr>';
     }
+    return $raids;
 }
--- js/update.js
+++ js/update.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     let page = getParameterByName('page');
-    setInterval(function() {
-        update(page);
-    }, 15000);
+    if (page !== '') {
+        countdown();
+        setInterval(function() {
+            update(page);
+        }, updateTime * 1000);
+    }
 });
 
 function getParameterByName(name, url) {
@@ -17,8 +20,68 @@ function getParameterByName(name, url) {
 
 function update(page) {
     $.get('./functions/api.php', {
-        page: page
+        type: page,
+        update: '1'
     }, function(data) {
-        /*todo add to datatable */
+        data = JSON.parse(data);
+        data = data.data;
+        if (data.length > 0) {
+            if (page == 'raids') {
+                window.raidTable.rows.add(data);
+            }
+        }
     });
+}
+
+function countdown(table) {
+    if (table === 'mon') {
+        table = monTable;
+    } else if (table === 'raid') {
+        table = raidTable;
+    }
+    $('.countdown')
+        .each(function() {
+            var timestamp = $(this).data('timestamp');
+            var timeOne = new Date(timestamp * 1000);
+            var timeNow = new Date();
+            if (timeOne <= timeNow) {
+                table.row($(this).closest('tr')).remove().draw();
+                return false;
+            }
+            $(this)
+                .countdown(timestamp * 1000, function(event) {
+                    var timer = event.strftime('%M:%S');
+                    $(this)
+                        .html('<span>' + timer + '</span><span>Remaining</span>');
+                })
+                .on('update.countdown', function(event) {
+                    var min = event.strftime('%M');
+                    min = parseInt(min);
+                    if (min >= 15) {
+                        $(this)
+                            .addClass('text-success');
+                    } else if (min >= 6 && min <= 15) {
+                        $(this)
+                            .addClass('text-warning');
+                        if ($(this)
+                            .hasClass('text-success')) {
+                            $(this)
+                                .removeClass('text-success');
+                        }
+                    } else if (min <= 6) {
+                        $(this)
+                            .addClass('text-danger');
+                        if ($(this)
+                            .hasClass('text-warning')) {
+                            $(this)
+                                .removeClass('text-warning');
+                        }
+                    }
+                })
+                .on('finish.countdown', function() {
+                    table.row($(this).closest('tr')).remove().draw();
+                });
+        });
+
+
 }
